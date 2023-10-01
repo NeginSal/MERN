b81494ea395c5fa7041dbb415d957c4fbc9358b1
NeginSal <neginsaljooghi@gmail.com>
2023-10-01 16:40:11 +0330

    feat:connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
 require('dotenv').config()
 
-const express = require('express');
-const todosRoutes = require('./routes/todos')
+const express = require('express')
+const mongoose = require('mongoose')
+const todoRoutes = require('./routes/todos')
 
 // express app
-const app = express();
+const app = express()
 
 // middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
-app.use('/api/todos', todosRoutes)
+app.use('/api/todos', todoRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port 4000')
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
