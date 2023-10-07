9ca5ed8d5fe0b5ca9543cc4d2280672b1a3e7cf6
NeginSal <neginsaljooghi@gmail.com>
2023-10-07 10:41:14 +0330

    feat:Added Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 
 //components
 import Home from './pages/Home'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
