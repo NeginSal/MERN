f66d4d1b507ca1fd81d0f6c3d76f2371e23aba98
NeginSal <neginsaljooghi@gmail.com>
2023-10-08 09:55:36 +0330

    feat:Added fetch todos data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
+
+
 const Home = () => {
-  return ( 
-    <div className="text-red-900 text-center text-2xl">
-      HomePage
-    </div>
-   );
+  const [todos, setTodos] = useState(null);
+
+  const fetchTodos = async () => {
+    const response = await fetch('/api/todos');
+    const data = await response.json();
+
+    if (response.ok) {
+      setTodos(data)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  return (
+    <>
+      {todos && todos.map((todo) =>
+        <div key={todo._id}>
+          <p>title - {todo.title}</p>
+          <p>description - {todo.description}</p>
+          <p>number - {todo.number}</p>
+        </div>
+      )}
+    </>
+  );
 }
- 
+
 export default Home;
