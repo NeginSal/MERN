2cf7dc4d7492a86011c8f6c046a69cd902b10282
NeginSal <neginsaljooghi@gmail.com>
2023-10-19 12:00:19 +0330

    feat:Added Auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { TodosContextProvider } from './context/TodoContext';
+import { AuthContextProvider } from './context/AuthContext';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodosContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </TodosContextProvider>
+    <AuthContextProvider>
+      <TodosContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TodosContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
