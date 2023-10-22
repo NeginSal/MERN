d3c2f866c75d679524ac60cf747f0dfef970a51a
NeginSal <neginsaljooghi@gmail.com>
2023-10-22 16:37:30 +0330

    feat:Added useLogout hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout()
+  }
   return (
     <div>
       <Link to="/">ToDo List</Link>
-      <Link to="/login">Log in</Link>
-      <Link to="/signup"> Sign up</Link>
+      <div>
+        <Link to="/login">Log in</Link>
+        <Link to="/signup"> Sign up</Link>
+      </div>
+      <div>
+        <button onClick={handleClick}>Log Out</button>
+      </div>
     </div>
   );
 }
