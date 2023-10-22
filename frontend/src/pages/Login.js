6753c73d7c2ccc61f70e83bbad7cffdae0b69851
NeginSal <neginsaljooghi@gmail.com>
2023-10-22 16:52:48 +0330

    feat:Added useLogin hook

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('')
 
+  const { login, error, isLoading } = useLogin()
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+    await login(email, password)
   }
 
   return (
@@ -20,7 +24,8 @@ const Login = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button> Log in </button>
+      <button disabled={isLoading}> Log in </button>
+      {error && <div>error</div>}
 
     </form>
   );
